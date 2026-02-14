972bb3e3caca83bab9dfdfdea9f57f50037725d0
Rxyan810 <umarali24062005@gmail.com>
2026-02-14 21:29:02 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# ====================================================
+# KONFIGURASI AKUN IG - ISI DENGAN DATA LU BANGSAT! 🔥
+# ====================================================
+
+USERNAME_ANDA = "yeron374"  # GANTI: username akun dummy lu
+PASSWORD_ANDA = "yeron_1998"  # GANTI: password dummy lu
+TARGET_USERNAME = "hdrriii"  # GANTI: username temen lu
+
+BATAS_FOLLOWING = 100  # Turunin dulu biar aman
+DELAY_MIN = 3.0        # Naikin delay
+DELAY_MAX = 5.0
+# ============= PROXY SETTINGS =============
+USE_PROXY = True  # GANTI JADI TRUE!
+
+# Pilih salah satu proxy dari list:
+PROXY_HTTP = "http://143.42.66.91:8080"   # Coba pake port 8080
+PROXY_HTTPS = "http://143.42.66.91:8080"  # Sama kaya di atas
